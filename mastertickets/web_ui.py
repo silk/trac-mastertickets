ccfb92dacc565a857f85b548f40354e8f937fdea
Etienne PIERRE <etienne@famillepierre.fr>
2011-05-03 13:42:51 +0000

    Add options for the dependency graph rendering
    added closed_color and opened_color for the rendering color
    added graph_direction to configure the graphviz rankdir option

--- mastertickets/web_ui.py
+++ mastertickets/web_ui.py
@@ -11,7 +11,7 @@ from trac.web.chrome import ITemplateProvider, add_stylesheet, add_script, \
                             add_ctxtnav
 from trac.ticket.api import ITicketManipulator
 from trac.ticket.model import Ticket
-from trac.config import Option, BoolOption
+from trac.config import Option, BoolOption, ChoiceOption
 from trac.util.html import html, Markup
 from trac.util.compat import set, sorted, partial
 
@@ -32,6 +32,14 @@ class MasterTicketsModule(Component):
     use_gs = BoolOption('mastertickets', 'use_gs', default=False,
                         doc='If enabled, use ghostscript to produce nicer output.')
     
+    closed_color = Option('mastertickets', 'closed_color', default='green',
+        doc='Color of closed tickets')
+    opened_color = Option('mastertickets', 'opened_color', default='red',
+        doc='Color of opened tickets')
+
+    graph_direction = ChoiceOption('mastertickets', 'graph_direction', choices = ['TD', 'LR', 'DT', 'RL'],
+        doc='Direction of the dependency graph (TD = Top Down, DT = Down Top, LR = Left Right, RL = Right Left)')
+
     FIELD_XPATH = '//div[@id="ticket"]/table[@class="properties"]//td[@headers="h_%s"]/text()'
     fields = set(['blocking', 'blockedby'])
     
@@ -175,6 +183,7 @@ class MasterTicketsModule(Component):
         links = TicketLinks(self.env, tkt_id)
         
         g = graphviz.Graph()
+        g.attributes['rankdir'] = self.graph_direction
         
         node_default = g['node']
         node_default['style'] = 'filled'
@@ -190,7 +199,7 @@ class MasterTicketsModule(Component):
             tkt = link.tkt
             node = g[tkt.id]
             node['label'] = u'#%s'%tkt.id
-            node['fillcolor'] = tkt['status'] == 'closed' and 'green' or 'red'
+            node['fillcolor'] = tkt['status'] == 'closed' and self.closed_color or self.opened_color
             node['URL'] = req.href.ticket(tkt.id)
             node['alt'] = u'Ticket #%s'%tkt.id
             node['tooltip'] = tkt['summary']
